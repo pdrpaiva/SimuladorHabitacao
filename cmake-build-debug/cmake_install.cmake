b114b95b330cf29d0c726744931f84efe948b1d1
Pedro Paiva <96551783+pdrpaiva@users.noreply.github.com>
2023-12-06 15:51:47 +0000

    a

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/Utilizador/Desktop/Pedro/ISEC/2oAno/1oSemestre/POO/Trabalho
+# Install script for directory: C:/Users/Utilizador/Desktop/Pedro/ISEC/2oAno/1oSemestre/POO/SimularHabitacao
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/Utilizador/Desktop/Pedro/ISEC/2oAno/1oSemestre/POO/Trabalho/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/Utilizador/Desktop/Pedro/ISEC/2oAno/1oSemestre/POO/SimularHabitacao/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
